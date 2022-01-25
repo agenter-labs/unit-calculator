db70db735fba08038c6db219c6bec35f3519ef2b
Siraj <siraj@agenter.com>
2022-01-25 12:08:48 +0530

    BOOKS-3432 #added changes

---
+++ src/components/useOutsideAlert.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export function useOutsideAlerter(ref: any, setState: any) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setState(false);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,92 +1,108 @@
-import React from 'react';
-import './style.css';
+import React, { useRef, useState } from "react";
+import "./style.css";
+import { useOutsideAlerter } from "./components/useOutsideAlert";
 
 interface Unit {
-    title?: string;
-    unitFrom: string;
-    unitTo: string;
-    factor: number;
+  title?: string;
+  unitFrom: string;
+  unitTo: string;
+  factor: number;
 }
 
 interface CalculatorProps {
-    conversions: Array<Unit>;
-    onResult?: (value: number) => void;
+  conversions: Array<Unit>;
+  onResult?: (value: number) => void;
 }
 
 const Calculator = (props: CalculatorProps) => {
+  const [conversion, setConversion] = React.useState<Unit | null>(null);
+  const [swap, setSwap] = React.useState<boolean>(false);
+  const [result, setResult] = React.useState<number>(0);
+  const [input, setInput] = React.useState<number>(0);
+  const [showOptions, setShowOptions] = useState(false);
 
-    const [conversion, setConversion] = React.useState<Unit|null>(null);
-    const [swap, setSwap] = React.useState<boolean>(false);
-    const [result, setResult] = React.useState<number>(0);
-    const [input, setInput] = React.useState<number>(0);
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setShowOptions);
 
-    React.useEffect(() => {
-        calculate(input);
-    }, [swap])
+  React.useEffect(() => {
+    calculate(input);
+  }, [swap]);
 
-    React.useEffect(() => {
-        calculate(input);
-    }, [input])
+  React.useEffect(() => {
+    calculate(input);
+  }, [input]);
 
-    React.useEffect(() => {
-        if (props.onResult) {
-            props.onResult(result);
-        }
-    }, [result])
-
-    const onChange = (val: number) => {
-        setSwap(false);
-        setConversion((prev) => {
-            return props.conversions[val] ?? null;
-        })
+  React.useEffect(() => {
+    if (props.onResult) {
+      props.onResult(result);
     }
+  }, [result]);
 
-    const calculate = (val: number) => {
-        if (conversion) {
-            setResult(
-                swap ? val / conversion?.factor : val * conversion?.factor
-            )
-        }
+  const onChange = (val: number) => {
+    setSwap(false);
+    setShowOptions(!showOptions);
+    setConversion((prev) => {
+      return props.conversions[val] ?? null;
+    });
+  };
+
+  const calculate = (val: number) => {
+    if (conversion) {
+      setResult(swap ? val / conversion?.factor : val * conversion?.factor);
     }
+  };
 
-    return (
-        <div className='uc-box'>
-            <select onChange={(event) => {onChange(parseInt(event.target.value))}}>
-                <option>--Select Unit--</option>
-                {props.conversions.map((unit, index) => (
-                    <option key={index} value={index}>
-                    {unit.title} ({unit.unitFrom}, {unit.unitTo})
-                </option>
-                ))}
-            </select>
-            {conversion !== null &&
-            
-            <div className='uc-body'>
-
-                <button className='uc-swap-btn' onClick={() => setSwap((prev) => !prev)} >
-                    {
-                        swap ? 
-                        `${conversion?.unitFrom} -> ${conversion?.unitTo}` :
-                        `${conversion?.unitTo} -> ${conversion?.unitFrom}` 
-                    }
-                </button> <br/>
-
-                <label>{swap ? conversion?.unitTo : conversion?.unitFrom}</label>
-                <input 
-                    type='number' 
-                    className='uc-input'
-                    onChange={(event) => {
-                        setInput(parseFloat(event.target.value))
-                    }}
-                />
-
-                <span className=''>
-                     = {result} {swap ? conversion?.unitFrom : conversion?.unitTo}
-                </span>
-            </div>
-            }
+  return (
+    <div className='uc__box'>
+      <div className='select__box'>
+        <ul ref={wrapperRef}>
+          <li
+            onClick={() => setShowOptions(!showOptions)}
+            className='custom__select__box'>
+            --Select Unit--
+          </li>
+          {showOptions &&
+            props.conversions.map((unit, index) => (
+              <li
+                className='select__options'
+                key={index}
+                onClick={() => {
+                  onChange(index);
+                }}>
+                {unit.title} ({unit.unitFrom}, {unit.unitTo})
+              </li>
+            ))}
+        </ul>
+      </div>
+      {conversion !== null && (
+        <div className='uc__body'>
+          <button
+            className='primary__button ml__25'
+            onClick={() => setSwap((prev) => !prev)}>
+            {swap
+              ? `${conversion?.unitFrom} -> ${conversion?.unitTo}`
+              : `${conversion?.unitTo} -> ${conversion?.unitFrom}`}
+          </button>{" "}
+          <br />
+          <div className='unit__result'>
+            <label className='unit__label'>
+              {swap ? conversion?.unitTo : conversion?.unitFrom}
+            </label>
+            <input
+              type='number'
+              className='uc__input'
+              onChange={(event) => {
+                setInput(parseFloat(event.target.value));
+              }}
+            />
+            <span className='unit__value'>
+              = {result} {swap ? conversion?.unitFrom : conversion?.unitTo}
+            </span>
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Calculator;
